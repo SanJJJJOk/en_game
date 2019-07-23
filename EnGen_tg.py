1168ffb0c3a7050d4a69cd8f1a49eb1156c6905c
Alexander Baklanov <ABaklanov3@slb.com>
2019-07-23 17:10:03 +0500

    test

--- EnGen_tg.py
+++ EnGen_tg.py
@@ -19,6 +19,7 @@ bot.
 
 import logging
 import os.path
+from module1 import *
 
 from datetime import datetime
 from threading import Timer
@@ -32,31 +33,90 @@ logger = logging.getLogger(__name__)
 
 #Define a global variables
 answer_list = set()
-Game = False
-Players = {}
+Game = Holder()
+#Players = {}
+Secret = {}
 BestResults = {}
 Time_timer = 7200.0
 #Time_timer = 30.0
 
+def login(update, context):
+	if len(update.message.text)<7:
+	    update.message.reply_text('login is invalid')
+	if not update.message.chat.id in Game.players:
+	    Game.players[update.message.chat.id] = Player(update.message.chat.id)
+	    update.message.reply_text('nickname is successfully created')
+	else:
+	    update.message.reply_text('nickname is successfully changed')
+	Game.set_nickname(update.message.chat.id, update.message.text[7:])
+
+
+def start(update, context):
+	update.message.reply_text('at first create nickname with command ''/login NICK''. you can change your nickname whenever you want.')
+
+def play(update, context):
+    if not update.message.chat.id in Game.players:
+        start(update, context)
+    try:
+        int_message = int(update.message.text[6:])
+        answer = Game.play(update.message.chat.id, int_message)
+        update.message.reply_text(answer)
+    except:
+        update.message.reply_text('something wrong')
+        
+def handlr(update, context):
+    if not update.message.chat.id in Game.players:
+        start(update, context)
+    answer = Game.game(update.message.chat.id, update.message.text)
+    for txt in answer:
+        update.message.reply_text(txt)
+        
+def statistics(update, context):
+    answer = Game.show_stat()
+    for txt in answer:
+        update.message.reply_text(txt)
+        
+def help(update, context):
+    update.message.reply_text('1 step, register: ''/login NICK'', where NICK - your nickname')
+    update.message.reply_text('2 step, choose game: ''/play N'', where N - game number(now only 1 game is available)')
+    update.message.reply_text('3 step: play')
+        
+def gamehelp(update, context):
+    if not update.message.chat.id in Secret:
+        Secret[update.message.chat.id] = []
+    Secret[update.message.chat.id].append(update.message.text)
+    if not update.message.chat.id in Game.players:
+        start(update, context)
+    answer = Game.gamehelp(update.message.chat.id)
+    for txt in answer:
+        update.message.reply_text(txt)
+        
+def secret(update, context):
+    update.message.reply_text(update.message.chat.id)
+    for key, value in Secret:
+        update.message.reply_text(key)
+        update.message.reply_text(', '.join(value))
+        Secret[key] = []
+
 # Define a class for players in game
-class Player (object):
-	"""docstring"""
+#class Player (object):
+#	"""docstring"""
 
-	def __init__(self, id, nickname):
-		"""Constructor"""
-		self.id = id
-		self.nickname = nickname
-		self.answer = set()
-		self.answer_completed = set()
+#	def __init__(self, id, nickname):
+#		"""Constructor"""
+#		self.id = id
+#		self.nickname = nickname
+#		self.answer = set()
+#		self.answer_completed = set()
 
-	def set_answer(self, answer_in):
-		self.answer_completed.add(answer_in)
-		if not self.answer_completed.__contains__(answer_in):
-			self.answer_completed.add(answer_in)
+#	def set_answer(self, answer_in):
+#		self.answer_completed.add(answer_in)
+#		if not self.answer_completed.__contains__(answer_in):
+#			self.answer_completed.add(answer_in)
 
-	def show_stats(self):
-		score = len(self.answer_completed) - len(self.answer)
-		return([self.nickname,self.answer,score])
+#	def show_stats(self):
+#		score = len(self.answer_completed) - len(self.answer)
+#		return([self.nickname,self.answer,score])
 
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -159,121 +219,121 @@ class Player (object):
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def cheat(update, context):
-	global answer_list,Game,Players
-	update.message.reply_text(len(answer_list))
-	update.message.reply_text(Game)
-	update.message.reply_text(len(Players))
-	for player in Players.values():
-		update.message.reply_text('_______________')
-		update.message.reply_text(player.nickname)
-		for ans in player.answer:
-			update.message.reply_text(ans)
-		update.message.reply_text('---')
-		for ans in player.answer_completed:
-			update.message.reply_text(ans)
+#def cheat(update, context):
+#	global answer_list,Game,Players
+#	update.message.reply_text(len(answer_list))
+#	update.message.reply_text(Game)
+#	update.message.reply_text(len(Players))
+#	for player in Players.values():
+#		update.message.reply_text('_______________')
+#		update.message.reply_text(player.nickname)
+#		for ans in player.answer:
+#			update.message.reply_text(ans)
+#		update.message.reply_text('---')
+#		for ans in player.answer_completed:
+#			update.message.reply_text(ans)
 
-def error(update, context):
-	logger.warning('Update "%s" caused error "%s"', update, context.error)
+#def error(update, context):
+#	logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-def login(update, context):
-	global answer_list,Game,Players
-	if len(update.message.text)>7:
-	    Players[update.message.chat.id] = [True, True, True, True, True, True, True]#Player(update.message.chat.id, update.message.text[7:])
-	    #update.message.reply_text(update.message.chat.id)
+#def login(update, context):
+#	global answer_list,Game,Players
+#	if len(update.message.text)>7:
+#	    Players[update.message.chat.id] = [True, True, True, True, True, True, True]#Player(update.message.chat.id, update.message.text[7:])
+#	    #update.message.reply_text(update.message.chat.id)
         
-def add_answers(update, context):
-	global answer_list,Game,Players
-	if not Game and len(update.message.text) > 5 and Players.__contains__(update.message.chat.id):
-		splitted = update.message.text[5:].split(' ')
-		str_answer = 'added:'
-		for code_answer in splitted :
-			if not Players[update.message.chat.id].answer.__contains__(code_answer) :
-				Players[update.message.chat.id].answer.add(code_answer)
-				Players[update.message.chat.id].answer_completed.add(code_answer)
-				str_answer+='\n'+code_answer
-		update.message.reply_text(str_answer)
+#def add_answers(update, context):
+#	global answer_list,Game,Players
+#	if not Game and len(update.message.text) > 5 and Players.__contains__(update.message.chat.id):
+#		splitted = update.message.text[5:].split(' ')
+#		str_answer = 'added:'
+#		for code_answer in splitted :
+#			if not Players[update.message.chat.id].answer.__contains__(code_answer) :
+#				Players[update.message.chat.id].answer.add(code_answer)
+#				Players[update.message.chat.id].answer_completed.add(code_answer)
+#				str_answer+='\n'+code_answer
+#		update.message.reply_text(str_answer)
 
-def show_my_ans(update, context):
-	global answer_list,Game,Players
-	if not Game and Players.__contains__(update.message.chat.id):
-		str_answer = 'full list:'
-		for ans in Players[update.message.chat.id].answer :
-			str_answer+='\n'+ans
-		update.message.reply_text(str_answer)
+#def show_my_ans(update, context):
+#	global answer_list,Game,Players
+#	if not Game and Players.__contains__(update.message.chat.id):
+#		str_answer = 'full list:'
+#		for ans in Players[update.message.chat.id].answer :
+#			str_answer+='\n'+ans
+#		update.message.reply_text(str_answer)
 
-def start_game(update, context):
-	global answer_list,Game,Players
-	Game = True
-	for player in Players.values():
-		for ans in player.answer:
-			answer_list.add(ans)
-	update.message.reply_text('success')
+#def start_game(update, context):
+#	global answer_list,Game,Players
+#	Game = True
+#	for player in Players.values():
+#		for ans in player.answer:
+#			answer_list.add(ans)
+#	update.message.reply_text('success')
 
-def echo(update, context):
-	global answer_list,Game,Players
-	if Game and update.message.text.startswith('.') and update.message.chat.id > 0:
-			answer_for_check = update.message.text[1:].strip()
-			if answer_for_check in answer_list:
-				update.message.reply_text('+')
-				Players[update.message.chat.id].set_answer(answer_for_check)
-			else:
-				update.message.reply_text('-')
+#def echo(update, context):
+#	global answer_list,Game,Players
+#	if Game and update.message.text.startswith('.') and update.message.chat.id > 0:
+#			answer_for_check = update.message.text[1:].strip()
+#			if answer_for_check in answer_list:
+#				update.message.reply_text('+')
+#				Players[update.message.chat.id].set_answer(answer_for_check)
+#			else:
+#				update.message.reply_text('-')
 
-def stats(update, context):
-	total_answer = len(answer_list)
-	result = ''
-	for player in Players.values():
-		result += '{}: {}/{}\n'.format(player.show_stats()[0], player.show_stats()[-1], total_answer - len(player.answer))
-	update.message.reply_text(result)
+#def stats(update, context):
+#	total_answer = len(answer_list)
+#	result = ''
+#	for player in Players.values():
+#		result += '{}: {}/{}\n'.format(player.show_stats()[0], player.show_stats()[-1], total_answer - len(player.answer))
+#	update.message.reply_text(result)
 
-def handlr(update, context):
-    if not update.message.chat.id in Players:
-        Players[update.message.chat.id] = [True, True, True, True, True, True, True]
-    id = update.message.chat.id
-    vals = Players[id]
-    text = update.message.text
-    if not len(text)==2:
-        if text=="0":
-            for i in range(7):
-                vals[i] = True
-            update.message.reply_text('0')
-        show(update, id)
-        return
-    try:
-        i1 = int(text[0])
-        i2 = int(text[1])
-        vals[i1-1]=False
-        vals[i2-1]=False
-        show(update, id)
-        tmp = [False, False, False, False, False, False, False]
-        for i in range(7):
-            if vals[i]:
-                if i == 0:
-                    tmp[1]=True
-                    tmp[6]=True
-                    continue
-                if i == 6:
-                    tmp[0]=True
-                    tmp[5]=True
-                    continue
-                tmp[i-1]=True
-                tmp[i+1]=True
-        Players[id] = tmp
-        update.message.reply_text('+')
-        show(update, id)
-    except:
-        show(update, id)
+#def handlr(update, context):
+#    if not update.message.chat.id in Players:
+#        Players[update.message.chat.id] = [True, True, True, True, True, True, True]
+#    id = update.message.chat.id
+#    vals = Players[id]
+#    text = update.message.text
+#    if not len(text)==2:
+#        if text=="0":
+#            for i in range(7):
+#                vals[i] = True
+#            update.message.reply_text('0')
+#        show(update, id)
+#        return
+#    try:
+#        i1 = int(text[0])
+#        i2 = int(text[1])
+#        vals[i1-1]=False
+#        vals[i2-1]=False
+#        show(update, id)
+#        tmp = [False, False, False, False, False, False, False]
+#        for i in range(7):
+#            if vals[i]:
+#                if i == 0:
+#                    tmp[1]=True
+#                    tmp[6]=True
+#                    continue
+#                if i == 6:
+#                    tmp[0]=True
+#                    tmp[5]=True
+#                    continue
+#                tmp[i-1]=True
+#                tmp[i+1]=True
+#        Players[id] = tmp
+#        update.message.reply_text('+')
+#        show(update, id)
+#    except:
+#        show(update, id)
 
 
-def show(update, id):
-    strr = ''
-    for i in range(7):
-        if Players[id][i]:
-            strr=strr+str(i+1)
-        else:
-            strr=strr+'x'
-    update.message.reply_text(strr)
+#def show(update, id):
+#    strr = ''
+#    for i in range(7):
+#        if Players[id][i]:
+#            strr=strr+str(i+1)
+#        else:
+#            strr=strr+'x'
+#    update.message.reply_text(strr)
 
 
 def main():
@@ -305,9 +365,12 @@ def main():
 
     #dp.add_handler(CommandHandler("add", add_answers))
     dp.add_handler(CommandHandler("login", login))
-    #dp.add_handler(CommandHandler("go", start_game))
-    #dp.add_handler(CommandHandler("stats", stats))
-    ##dp.add_handler(CommandHandler("cheat123", cheat))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("play", play))
+    dp.add_handler(CommandHandler("statistics", statistics))
+    dp.add_handler(CommandHandler("gamehelp", gamehelp))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("secret", secret))
     #dp.add_handler(CommandHandler("show", show_my_ans))
     #dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_handler(MessageHandler(Filters.text, handlr))
