a1ea074ec8bce2ae08a2b8f49aa5738d2d97a610
q <q>
2019-11-18 11:39:58 +0500

    a

--- EnGen_tg.py
+++ EnGen_tg.py
@@ -46,7 +46,7 @@ Holder = SettingsHolder()
 #tg methods
 
 def tg_error(update, context):
-    update.message.reply_text("Error: {0}".format(str(e)))
+    update.message.reply_text("Error: {0}".format(str(context.error)))
 
 def tg_test(update, context):
     newstr = ""
