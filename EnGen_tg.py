6c152e06cf2f5409b06496ce3df928023bee4f8b
Alexander Baklanov <ABaklanov3@slb.com>
2020-08-20 19:04:41 +0500

    some

--- EnGen_tg.py
+++ EnGen_tg.py
@@ -30,8 +30,9 @@ import re
 import json
 import time
 import base64
-from datetime import datetime, date, time, timedelta
+from datetime import datetime, date, timedelta
 
+from threading import Thread
 from threading import Timer
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -47,124 +48,8 @@ def tg_error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
     
 Holder = GlobalInfo()
-
-class TgCommands:
-    Move = 'move'
-    Del = 'del'
-    Add = 'add'
-    FastAdd = 'fastadd'
-    QuickDel = 'quickdel'
-    Money = 'money'
-    Help = 'help'
-    Count = 'count'
-    Fullstat = 'fullstat'
-    Stat = 'stat'
-    
-#def is_meta_check(word1, word2):
-#    if len(word1) != len(word2):
-#        return False
-#    counter = 0
-#    for i in range(0, len(word1)):
-#        if word1[i]!=word2[i]:
-#            counter+=1
-#    return counter == 1
-
-#def is_logo_check(word1, word2):
-#    long_word = ''
-#    short_word = ''
-#    if len(word1) == len(word2) + 1:
-#        long_word = word1
-#        short_word = word2
-#    else:
-#        if len(word2) == len(word1) + 1:
-#            long_word = word2
-#            short_word = word1
-#        else:
-#            return False
-#    diff_index = 0
-#    for i in range(0, len(short_word)):
-#        if long_word[i]!=short_word[i]:
-#            diff_index = i
-#            break
-#    for i in range(diff_index, len(short_word)):
-#        if long_word[i + 1]!=short_word[i]:
-#            diff_index = -1
-#            break
-#    return diff_index!=-1 or long_word.startswith(short_word)
-
-#def smart_check(word, symbol):
-#    if word in GlobalMonopoly.holder:
-#        return None
-#    for exist_word in GlobalMonopoly.holder:
-#        if is_meta_check(exist_word, word) or is_logo_check(exist_word, word):
-#            exist_symbols = [i.symbol for i in GlobalMonopoly.holder[exist_word]]
-#            if symbol in exist_symbols:
-#                return SmartResult(word, exist_word, True)
-#            return SmartResult(word, exist_word, False)
-#    return None
-
-
-
-
-
-            #last_action = GlobalMonopoly.last_ations[update.message.from_user.id]
-            #if not last_action is None:
-            #    if last_action.action_type == ActionTypes.TypoMiss and last_action.msg_id == reply_msg.id:
-            #        if update.message.text.startswith('д'):
-            #            del GlobalMonopoly.holder[last_action.some_field.added_word]
-            #            if not last_action.action_symbol in GlobalMonopoly.holder[last_action.action_word]:
-            #                GlobalMonopoly.holder[last_action.action_word].append(last_action.action_symbol)
-            #            update.message.reply_text('сделано')
-            #            return
-            #        if update.message.text.startswith('н'):
-            #            GlobalMonopoly.last_ations[update.message.from_user.id] = None
-            #            update.message.reply_text('ок, буду молчать в тряпочку')
-            #            return
-            #        update.message.reply_text('ну ёба, да или нет скажи, че ты мне пишешь?')
-            #        return
-        
-                    #smart_check_result = smart_check(input_word, input_symbol)
-                    #if smart_check_result is None:
-                    #    add_result = add_word_symbol(input_word, input_symbol)
-                    #    if add_result is None:
-                    #        update.message.reply_text(Result.Result1 + 'было уже')
-                    #    else:
-                    #        update.message.reply_text(Result.Result0 + 'опачки, уже ' + str(add_result) + ' штуки')
-                    #else:
-                    #    add_result = add_word_symbol(input_word, input_symbol)
-                    #    if smart_check_result.is_symbol_exist:
-                    #        GlobalMonopoly.last_ations[update.message.from_user.id] = ActionItem(update.message.from_user.id, update.message.message_id, is_group_msg, text, input_word, input_symbol, smart_check_result, ActionTypes.TypoMiss)
-                    #        update.message.reply_text('я, конечно, добавил "' + input_word  + '", но походу ты имел в виду "' + smart_check_result + '". причем "' + symbol + '" там уже существует. ответь реплаем на это сообщение да если хочешь исправить, нет - если не хочешь. или просто проигнорируй')
-                    #    else:
-                    #        GlobalMonopoly.last_ations[update.message.from_user.id] = ActionItem(update.message.from_user.id, update.message.message_id, is_group_msg, text, input_word, input_symbol, smart_check_result, ActionTypes.TypoMiss)
-                    #        update.message.reply_text('я, конечно, добавил "' + input_word  + '", но походу ты имел в виду "' + smart_check_result + '". ответь реплаем на это сообщение да если хочешь исправить, нет - если не хочешь. или просто проигнорируй')
-
-
-        #if text.startswith('+'):
-        #    int_value = get_int(text[1:])
-        #    if int_value is None:
-        #        update.message.reply_text('эт не число')
-        #        return
-        #    GlobalMonopoly.money = GlobalMonopoly.money + int_value
-        #    update.message.reply_text('добавлено ' + str(int_value) + ', осталось ' + str(GlobalMonopoly.money))
-        #    return
-
-        #if text.startswith('-'):
-        #    int_value = get_int(text[1:])
-        #    if int_value is None:
-        #        update.message.reply_text('эт не число')
-        #        return
-        #    GlobalMonopoly.money = GlobalMonopoly.money - int_value
-        #    update.message.reply_text('вычтено ' + str(int_value) + ', осталось ' + str(GlobalMonopoly.money))
-        #    return
-
-def get_small_info(word):
-    if not word in GlobalMonopoly.holder:
-        return None
-    count = len(GlobalMonopoly.holder[word])
-    new_symbols = [i.symbol for i in GlobalMonopoly.holder[word]]
-    new_symbols.sort()
-    return word + ' уже ' + str(count) + ' штук(' + ', '.join(new_symbols) + ')'
+Is_monitoring_active = False
+Output_dict = {}
 
 def get_int(text):
     try:
@@ -172,62 +57,60 @@ def get_int(text):
     except Exception as e:
         return None
 
-def tg_p_default(update, context):
+def tg_update(update, context, url):
+    global Is_monitoring_active
     try:
-        text = update.message.text.lower()
+        while Is_monitoring_active:
+            #fp = request.urlopen("http://m.kurgan.en.cx/GameBonusPenaltyTime.aspx?tid=10301&level=0&gid=68107")
+            fp = request.urlopen(url)
+            mybytes = fp.read()
+
+            mystr = mybytes.decode("utf8")
+            fp.close()
+
+            soup = BeautifulSoup(mystr, 'html.parser')
+            regex_t = re.compile("^PlayersRepeater")
+            find_text1 = soup.find_all('tr', {'id': regex_t})
+
+            reply_str = ''
+            for i in find_text1:
+                listt = [ ii for ii in i.children if ii.name=='td' ]
+                st_time = listt[0].get_text()
+                st_text = listt[-1].get_text()
+                if not st_time in output_dict:
+                    reply_str = reply_str + '\n' + st_text
+                    output_dict[st_time] = st_text
+
+
+
+
+            update.message.reply_text('q')
+            time.sleep(10)
+        update.message.reply_text("i'm off")
     except Exception as e:
         err_msg = "неизвестная ошибка: {0}".format(str(e))
         update.message.reply_text(err_msg)
-        context.bot.send_message('228485598', err_msg + 'id:' + str(update.message.chat.id))
-       
-def tg_p_stat(update, context):
+
+def tg_go(update, context):
+    global Is_monitoring_active
     try:
-        #fp = request.urlopen("http://m.kurgan.en.cx/GameStat.aspx?gid=68107")
-        output_dict = {}
-        fp = request.urlopen(update.message.text[6:])
-        mybytes = fp.read()
-
-        mystr = mybytes.decode("utf8")
-        fp.close()
-
-        soup = BeautifulSoup(mystr, 'html.parser')
-        regex_m = re.compile("^totalCell")
-        find_text3 = soup.find_all("td", {'class': regex_m})
-
-        for i in find_text3:
-            txtitem = i.get_text()
-            if 'бонус' in txtitem:
-                ind = txtitem.index('бонус')
-                txtbp = txtitem[ind+5:].replace(' ','')
-                ttertert = get_count_sec(txtbp)
-                team_name = i.find("a").get_text()
-                output_dict[team_name] = ttertert
-            if 'штраф' in txtitem:
-                ind = txtitem.index('штраф')
-                txtbp = txtitem[ind+5:].replace(' ','')
-                ttertert = get_count_sec(txtbp)
-                team_name = i.find("a").get_text()
-                output_dict[team_name] = -ttertert
-        
-        otwt_val = -1
-        for key in output_dict:
-            if 'Win Team' in key:
-                otwt_val = output_dict[key]
-        for key in output_dict:
-            output_dict[key] = output_dict[key] - otwt_val
-
-        output_sorted = {}
-        list_d = list(output_dict.items())
-        list_d.sort(key=lambda i: i[1], reverse=True)
-        output_str = ''
-        for i in list_d:
-            output_str = output_str + '\n' +  str(i[1]) + '-' + i[0]
-        update.message.reply_text(output_str)
-
+        Is_monitoring_active = True
+        url = update.message.text[4:]
+        x = Thread(target=tg_update, args=(update,context,url,))
+        x.start()
+        update.message.reply_text('started')
     except Exception as e:
         err_msg = "неизвестная ошибка: {0}".format(str(e))
         update.message.reply_text(err_msg)
-        context.bot.send_message('228485598', err_msg + 'id:' + str(update.message.chat.id))
+
+def tg_stop(update, context):
+    global Is_monitoring_active
+    try:
+        Is_monitoring_active = False
+        update.message.reply_text('stopped')
+    except Exception as e:
+        err_msg = "неизвестная ошибка: {0}".format(str(e))
+        update.message.reply_text(err_msg)
 
 def get_count_sec(input_str):
     output_sec = 0
@@ -249,51 +132,51 @@ def get_count_sec(input_str):
 
 def main():
     #fp = request.urlopen("http://m.kurgan.en.cx/GameBonusPenaltyTime.aspx?tid=10301&level=0&gid=68107")
-    #mybytes = fp.read()
-
-    #mystr = mybytes.decode("utf8")
-    #fp.close()
-
-    #file = open('123.txt', 'w', encoding='utf-8')
-    #file.truncate(0)
-    #file.write(mystr)
-    #file.close()
-
+    #fp = request.urlopen("http://m.kurgan.en.cx/GameBonusPenaltyTime.aspx?gid=68107")
     #fp = request.urlopen("http://m.kurgan.en.cx/GameStat.aspx?gid=68107")
     #mybytes = fp.read()
 
     #mystr = mybytes.decode("utf8")
     #fp.close()
 
-    #file = open('124.txt', 'w', encoding='utf-8')
+    #file = open('125.txt', 'w', encoding='utf-8')
     #file.truncate(0)
     #file.write(mystr)
     #file.close()
 
-    #file = open('124.txt', 'r', encoding='utf-8')
-    #mystr = file.read()
-    #file.close()
-    #output_dict = {}
+    
+    file = open('124.txt', 'r', encoding='utf-8')
+    mystr_stat = file.read()
+    file.close()
 
-    #soup = BeautifulSoup(mystr, 'html.parser')
-    #regex_m = re.compile("^totalCell")
-    #find_text3 = soup.find_all("td", {'class': regex_m})
 
-    #for i in find_text3:
-    #    txtitem = i.get_text()
-    #    if 'бонус' in txtitem:
-    #        ind = txtitem.index('бонус')
-    #        txtbp = txtitem[ind+5:].replace(' ','')
-    #        ttertert = get_count_sec(txtbp)
-    #        team_name = i.find("a").get_text()
-    #        output_dict[team_name] = ttertert
-    #    if 'штраф' in txtitem:
-    #        ind = txtitem.index('штраф')
-    #        txtbp = txtitem[ind+5:].replace(' ','')
-    #        ttertert = get_count_sec(txtbp)
-    #        team_name = i.find("a").get_text()
-    #        output_dict[team_name] = -ttertert
+    file = open('125.txt', 'r', encoding='utf-8')
+    mystr = file.read()
+    file.close()
+    output_dict = {}
 
+    soup = BeautifulSoup(mystr_stat, 'html.parser')
+    regex_m = re.compile("^totalCell")
+    find_text3 = soup.find_all("td", {'class': regex_m})
+
+    #------------------------------------------top team------------------------------------------
+    list_top4 = []
+    top_team = find_text3[0].find("a").get_text()
+    if not 'Win Team' in top_team:
+        list_top4.append(top_team)
+        
+    top_team = find_text3[2].find("a").get_text()
+    if not 'Win Team' in top_team:
+        list_top4.append(top_team)
+        
+    top_team = find_text3[4].find("a").get_text()
+    if not 'Win Team' in top_team:
+        list_top4.append(top_team)
+        
+    top_team = find_text3[6].find("a").get_text()
+    if not 'Win Team' in top_team:
+        list_top4.append(top_team)
+    #------------------------------------------top team------------------------------------------
     #otwt_val = -1
     #for key in output_dict:
     #    if 'Win Team' in key:
@@ -308,23 +191,41 @@ def main():
     #for i in list_d:
     #    output_str = output_str + '\n' +  str(i[1]) + '-' + i[0]
 
-    #soup = BeautifulSoup(mystr, 'html.parser')
-    #regex_t = re.compile("^PlayersRepeater")
-    #find_text1 = soup.find_all('tr', {'id': regex_t})
+    soup = BeautifulSoup(mystr, 'html.parser')
+    regex_t = re.compile("^PlayersRepeater")
+    find_text1 = soup.find_all('tr', {'id': regex_t})
 
-    #for i in find_text1:
-    #    listt = [ ii for ii in i.children if ii.name=='td' ]
-    #    st_time = listt[0].get_text()
-    #    st_text = listt[-1].get_text()
-    #    if not st_time in output_dict:
-    #        #send message
-    #        output_dict[st_time] = st_text
+
+    count_bonuses = {}
+    count_penalties = {}
+    count_something = {}
+    for i in find_text1:
+        listt = [ ii for ii in i.children if ii.name=='td' ]
+        st_teamname = listt[1].get_text()
+        st_textbonus = listt[-2].get_text().lower()
+        if 'бонус' in st_textbonus:
+            if st_teamname in count_bonuses:
+                count_bonuses[st_teamname] = count_bonuses[st_teamname] + 1
+            else:
+                count_bonuses[st_teamname] = 1
+            continue
+        if 'штраф' in st_textbonus:
+            if st_teamname in count_penalties:
+                count_penalties[st_teamname] = count_penalties[st_teamname] + 1
+            else:
+                count_penalties[st_teamname] = 1
+            continue
+        if st_teamname in count_something:
+            count_something[st_teamname] = count_something[st_teamname] + 1
+        else:
+            count_something[st_teamname] = 1
 
     updater = Updater("408100374:AAEhMleUbdVH_G1xmKeCAy8MlNfyBwB9AOo", use_context=True)
     #updater = Updater("979411435:AAEHIVLx8L8CxmjIHtitaH4L1GeV_OCRJ7M", use_context=True)
     dp = updater.dispatcher
     
-    dp.add_handler(CommandHandler(TgCommands.Stat, tg_p_stat))
+    dp.add_handler(CommandHandler('go', tg_go))
+    dp.add_handler(CommandHandler('stop', tg_stop))
     #dp.add_handler(MessageHandler(Filters.text, tg_p_default))
     
     dp.add_error_handler(tg_error)
