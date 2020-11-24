0490d23a588e7fbba7e68d43f2b0ade89cd4c146
Alexander Baklanov <ABaklanov3@slb.com>
2020-11-24 19:35:57 +0500

    q

--- EnGen_tg.py
+++ EnGen_tg.py
@@ -43,14 +43,11 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 class TgCommands:
-    Auto = 'auto'
     Go = 'go'
-    Got = 'got'
     Stop = 'stop'
-    Set = 'set'
     Game = 'game'
-    Zeroon = 'zeroon'
-    Zerooff = 'zerooff'
+    LvlStat = 'lvlstat'
+    Points = 'points'
 
 class Emjs:
     First = '\ud83e\udd47'
@@ -61,22 +58,255 @@ class Emjs:
     Penalty = '\u274c'
     Point = '\ud83d\udccd'
 
+class ActionItem:
+    def __init__(self, e_time, e_team, e_lvl, e_bonus, e_score, e_itemid, e_user):
+        self.e_time = e_time
+        self.e_team = e_team
+        self.e_lvl = e_lvl
+        self.e_bonus = e_bonus
+        self.e_score = e_score
+        self.e_itemid = e_itemid
+        self.e_user = e_user
+
 def tg_error(update, context):
     err_msg = "Callback: {0}".format(str(context.error))
-    update.message.reply_text(err_msg)
     logger.warning('Update "%s" caused error "%s"', update, context.error)
     
 Holder = GlobalInfo()
 Is_monitoring_active = False
-Is_zeroarmormode_active = False
-Teams = []
-Output_arr = []
-Domain = 'demo'
-Gameid = '30837'
-Team1 = 'Win Team'
-Team2 = 'полоскун'
+Auto_update = True
+Output_arr = {}
+Domain = '72'
+Gameid = '70696'
+#Team1 = 'Win Team'
+#Team2 = 'полоскун'
 
-def tg_p_game(update, context):
+#def tg_set(update, context):
+#    global Is_monitoring_active, Teams, Domain, Gameid
+#    try:
+#        input_teams = update.message.text[len(TgCommands.Set)+2:].split(' ')
+        
+#        teams = []
+#        fp = request.urlopen("http://m."+Domain+".en.cx/GameStat.aspx?gid="+Gameid)
+#        mybytes = fp.read()
+
+#        mystr = mybytes.decode("utf8")
+#        fp.close()
+
+#        soup = BeautifulSoup(mystr, 'html.parser')
+#        regex_m = re.compile("^totalCell")
+#        find_text3 = soup.find_all("td", {'class': regex_m})
+    
+#        allteams = []
+#        for itemstat in find_text3:
+#            teamname = itemstat.find("a").get_text()
+#            if not teamname in allteams:
+#                allteams.append(teamname)
+
+#        for inputteam in input_teams:
+#            for team in allteams:
+#                if inputteam in team and not team in teams:
+#                    teams.append(team)
+
+#        Teams = teams
+#        update.message.reply_text('\n'.join(teams))
+#    except Exception as e:
+#        err_msg = "неизвестная ошибка: {0}".format(str(e))
+#        update.message.reply_text(err_msg)
+
+#def tg_auto_teams(update, context):
+#    global Is_monitoring_active, Teams, Domain, Gameid
+#    try:
+#        teams = []
+#        fp = request.urlopen("http://m."+Domain+".en.cx/GameStat.aspx?gid="+Gameid)
+#        mybytes = fp.read()
+
+#        mystr = mybytes.decode("utf8")
+#        fp.close()
+
+#        soup = BeautifulSoup(mystr, 'html.parser')
+#        regex_m = re.compile("^totalCell")
+#        find_text3 = soup.find_all("td", {'class': regex_m})
+    
+#        #teamname = find_text3[1].find("a").get_text()
+#        #if not Team1 in teamname and not Team2 in teamname:
+#        #    teams.append(teamname)
+#        #teamname = find_text3[3].find("a").get_text()
+#        #if not Team1 in teamname and not Team2 in teamname:
+#        #    teams.append(teamname)
+#        #teamname = find_text3[5].find("a").get_text()
+#        #if not Team1 in teamname and not Team2 in teamname:
+#        #    teams.append(teamname)
+#        Teams = teams
+#        update.message.reply_text('\n'.join(teams))
+#    except Exception as e:
+#        err_msg = "неизвестная ошибка: {0}".format(str(e))
+#        update.message.reply_text(err_msg)
+
+
+
+#---------------------------------------def---------------------------------------------------------
+
+def download_data():
+    global Is_monitoring_active, Auto_update, Output_arr, Domain, Gameid
+    fp = request.urlopen("http://"+Domain+".en.cx/GameBonusPenaltyTime.aspx?gid="+Gameid)
+    mybytes = fp.read()
+
+    mystr = mybytes.decode("utf8")
+    fp.close()
+
+    soup = BeautifulSoup(mystr, 'html.parser')
+    regex_t = re.compile("^PlayersRepeater")
+    regex_zero = re.compile("([0-9]+)с")
+    find_text1 = soup.find_all('tr', {'id': regex_t})
+
+    action_items = []
+
+    #TODO:
+
+    
+            #fp = request.urlopen("http://m."+Domain+".en.cx/GameBonusPenaltyTime.aspx?gid="+Gameid)
+            #mybytes = fp.read()
+
+            #mystr = mybytes.decode("utf8")
+            #fp.close()
+
+            #soup = BeautifulSoup(mystr, 'html.parser')
+            #regex_t = re.compile("^PlayersRepeater")
+            #regex_zero = re.compile("([0-9]+)с")
+            #find_text1 = soup.find_all('tr', {'id': regex_t})
+
+            #reply_str = ''
+            #zero_str = ''
+            #for i in find_text1:
+            #    listt = [ ii for ii in i.children if ii.name=='td' ]
+            #    st_team = listt[1].get_text()
+            #    st_time = listt[0].get_text()
+            #    st_key = st_team + st_time
+            #    if not st_key in Output_arr:
+            #        if st_team in Teams:
+            #            st_text = listt[-1].get_text()
+
+    return action_items
+
+def update_data(action_items):
+    global Is_monitoring_active, Auto_update, Output_arr, Domain, Gameid
+    new_items_ids = []
+    for item in action_items:
+        if not item.e_itemid in Output_arr:
+            Output_arr[item.e_itemid] = item
+            new_items_ids.append(item.e_itemid)
+    return new_items_ids
+
+#-------------------------------------tg-run-------------------------------------------------------
+
+def tg_run(update, context):
+    global Is_monitoring_active, Auto_update, Output_arr, Domain, Gameid
+    try:
+        while Is_monitoring_active:
+            action_items = download_data()
+            new_items_ids = update_data(action_items)
+            for id in new_items_ids:
+                item = Output_arr[id]
+                reply_str = reply_str + '\n'
+                if item.e_bonus and item.e_score==1:
+                    reply_str = reply_str + Emjs.Point
+                else:
+                    if item.e_bonus:
+                        reply_str = reply_str + Emjs.Bonus
+                    else:
+                        reply_str = reply_str + Emjs.Penalty
+                    reply_str = reply_str + '(' + item.e_score + 's)\n'
+                reply_str = reply_str + item.e_team + ' at ' + str(item.e_time) + ' by ' + item.e_user
+            if not reply_str=='':
+                print_long(update, context, reply_str)
+            time.sleep(60)
+        update.message.reply_text("i'm off")
+    except Exception as e:
+        err_msg = "неизвестная ошибка update: {0}".format(str(e))
+        update.message.reply_text(err_msg)
+        context.bot.send_message('228485598', err_msg + 'id:' + str(update.message.chat.id))
+        
+#---------------------------------------tg---------------------------------------------------------
+
+def tg_points_stat(update, context):
+    global Is_monitoring_active, Auto_update, Output_arr, Domain, Gameid
+    try:
+        input_int = int(update.message.text[len(TgCommands.Points)+2:])
+        if Auto_update:
+            action_items = download_data()
+            new_items_ids = update_data(action_items)
+        items = [Output_arr[key] for key in Output_arr]
+        output = {}
+        for item in items:
+            if item.e_user in output:
+                output[item.e_user] = output[item.e_user] + item.e_score
+            else:
+                output[item.e_user] = item.e_score
+
+        output_sorted = list(output.items())
+        output_sorted.sort(key=lambda i: i[1], reverse=True)
+        reply_str=''
+        for i in output_sorted:
+            reply_str = reply_str + i[0] + '-' + i[1]
+        if not reply_str=='':
+            print_long(update, context, reply_str)
+    except Exception as e:
+        err_msg = "неизвестная ошибка update: {0}".format(str(e))
+        update.message.reply_text(err_msg)
+        context.bot.send_message('228485598', err_msg + 'id:' + str(update.message.chat.id))
+
+def tg_lvl_stat(update, context):
+    global Is_monitoring_active, Auto_update, Output_arr, Domain, Gameid
+    try:
+        input_int = int(update.message.text[len(TgCommands.LvlStat)+2:])
+        if Auto_update:
+            action_items = download_data()
+            new_items_ids = update_data(action_items)
+        items = [Output_arr[key] for key in Output_arr if Output_arr[key].e_lvl==input_int]
+        output = {}
+        for item in items:
+            if item.e_team in output:
+                output[item.e_team] = output[item.e_team] + item.e_score
+            else:
+                output[item.e_team] = item.e_score
+        output_sorted = list(output.items())
+        output_sorted.sort(key=lambda i: i[1], reverse=True)
+        reply_str=''
+        for i in output_sorted:
+            reply_str = reply_str + i[0] + '-' + i[1]
+        if not reply_str=='':
+            print_long(update, context, reply_str)
+    except Exception as e:
+        err_msg = "неизвестная ошибка update: {0}".format(str(e))
+        update.message.reply_text(err_msg)
+        context.bot.send_message('228485598', err_msg + 'id:' + str(update.message.chat.id))
+
+def tg_go(update, context):
+    global Is_monitoring_active
+    try:
+        Is_monitoring_active = True
+        action_items = download_data()
+        new_items_ids = update_data(action_items)
+        x = Thread(target=tg_run, args=(update,context,))
+        x.start()
+        update.message.reply_text('started')
+    except Exception as e:
+        err_msg = "неизвестная ошибка: {0}".format(str(e))
+        update.message.reply_text(err_msg)
+        context.bot.send_message('228485598', err_msg + 'id:' + str(update.message.chat.id))
+
+def tg_stop(update, context):
+    global Is_monitoring_active
+    try:
+        Is_monitoring_active = False
+        update.message.reply_text('stopped')
+    except Exception as e:
+        err_msg = "неизвестная ошибка: {0}".format(str(e))
+        update.message.reply_text(err_msg)
+        context.bot.send_message('228485598', err_msg + 'id:' + str(update.message.chat.id))
+        
+def tg_game(update, context):
     global Domain, Gameid
     try:
         input = update.message.text[len(TgCommands.Game)+2:].split(' ')
@@ -88,183 +318,18 @@ def tg_p_game(update, context):
         update.message.reply_text(err_msg)
         context.bot.send_message('228485598', err_msg + 'id:' + str(update.message.chat.id))
 
-def tg_set(update, context):
-    global Is_monitoring_active, Teams, Domain, Gameid
-    try:
-        input_teams = update.message.text[len(TgCommands.Set)+2:].split(' ')
-        
-        teams = []
-        fp = request.urlopen("http://m."+Domain+".en.cx/GameStat.aspx?gid="+Gameid)
-        mybytes = fp.read()
-
-        mystr = mybytes.decode("utf8")
-        fp.close()
-
-        soup = BeautifulSoup(mystr, 'html.parser')
-        regex_m = re.compile("^totalCell")
-        find_text3 = soup.find_all("td", {'class': regex_m})
-    
-        allteams = []
-        for itemstat in find_text3:
-            teamname = itemstat.find("a").get_text()
-            if not teamname in allteams:
-                allteams.append(teamname)
-
-        for inputteam in input_teams:
-            for team in allteams:
-                if inputteam in team and not team in teams:
-                    teams.append(team)
-
-        Teams = teams
-        update.message.reply_text('\n'.join(teams))
-    except Exception as e:
-        err_msg = "неизвестная ошибка: {0}".format(str(e))
-        update.message.reply_text(err_msg)
-
-def tg_auto_teams(update, context):
-    global Is_monitoring_active, Teams, Domain, Gameid, Team1, Team2
-    try:
-        teams = []
-        fp = request.urlopen("http://m."+Domain+".en.cx/GameStat.aspx?gid="+Gameid)
-        mybytes = fp.read()
-
-        mystr = mybytes.decode("utf8")
-        fp.close()
-
-        soup = BeautifulSoup(mystr, 'html.parser')
-        regex_m = re.compile("^totalCell")
-        find_text3 = soup.find_all("td", {'class': regex_m})
-    
-        teamname = find_text3[1].find("a").get_text()
-        if not Team1 in teamname and not Team2 in teamname:
-            teams.append(teamname)
-        teamname = find_text3[3].find("a").get_text()
-        if not Team1 in teamname and not Team2 in teamname:
-            teams.append(teamname)
-        teamname = find_text3[5].find("a").get_text()
-        if not Team1 in teamname and not Team2 in teamname:
-            teams.append(teamname)
-        Teams = teams
-        update.message.reply_text('\n'.join(teams))
-    except Exception as e:
-        err_msg = "неизвестная ошибка: {0}".format(str(e))
-        update.message.reply_text(err_msg)
-
-def tg_update(update, context):
-    global Is_monitoring_active, Is_zeroarmormode_active, Teams, Output_arr, Domain, Gameid
-    try:
-        while Is_monitoring_active:
-            fp = request.urlopen("http://m."+Domain+".en.cx/GameBonusPenaltyTime.aspx?gid="+Gameid)
-            mybytes = fp.read()
-
-            mystr = mybytes.decode("utf8")
-            fp.close()
-
-            soup = BeautifulSoup(mystr, 'html.parser')
-            regex_t = re.compile("^PlayersRepeater")
-            regex_zero = re.compile("([0-9]+)с")
-            find_text1 = soup.find_all('tr', {'id': regex_t})
-
-            reply_str = ''
-            zero_str = ''
-            for i in find_text1:
-                listt = [ ii for ii in i.children if ii.name=='td' ]
-                st_team = listt[1].get_text()
-                st_time = listt[0].get_text()
-                st_key = st_team + st_time
-                if not st_key in Output_arr:
-                    if st_team in Teams:
-                        st_text = listt[-1].get_text()
-                        st_acttxt = listt[-2].get_text()
-                        reply_str = reply_str + '\n' + get_emjs(st_team, st_acttxt) + st_text
-                    if Is_zeroarmormode_active:
-                        st_textzero = listt[-1].get_text()
-                        zeromatch = regex_zero.findall(st_textzero)
-                        if len(zeromatch)==0:
-                            zero_str = zero_str + '\n' + '...хуита(==0)'
-                        else:
-                            if len(zeromatch)>1:
-                                zero_str = zero_str + '\n' + '...говно(>1)'
-                            else:
-                                intsecs = int(zeromatch[0])
-                                if intsecs>=60:
-                                    zero_str = zero_str + '\n' + st_textzero
-                    Output_arr.append(st_key)
-
-            if not reply_str=='':
-                print_long(update, context, reply_str)
-            if Is_zeroarmormode_active and not zero_str=='':
-                print_long(update, context, Emjs.Point + zero_str)
-            time.sleep(5)
-        update.message.reply_text("i'm off")
-    except Exception as e:
-        err_msg = "неизвестная ошибка update: {0}".format(str(e))
-        update.message.reply_text(err_msg)
-        
-def get_emjs(teamname, actiontxt):
-    global Is_monitoring_active, Teams, Output_arr, Domain, Gameid
-    teamemjs = Emjs.Other
-    if len(Teams)>0 and teamname==Teams[0]:
-        teamemjs = Emjs.First
-    if len(Teams)>1 and teamname==Teams[1]:
-        teamemjs = Emjs.Second
-    if len(Teams)>2 and teamname==Teams[2]:
-        teamemjs = Emjs.Third
-    if 'бонус' in actiontxt or 'bonus' in actiontxt:
-        return teamemjs + '-' + Emjs.Bonus
-    return teamemjs + '-' + Emjs.Penalty
+#-------------------------------------tghelp-------------------------------------------------------
 
 def print_long(update, context, input_text):
-    global Is_monitoring_active, Teams, Output_arr, Domain, Gameid
     if len(input_text) == 0:
-        context.bot.send_message('-442090041', '-')
+        update.message.reply_text('-')
         return
     if len(input_text) > 4096:
         for x in range(0, len(input_text), 4096):
-            context.bot.send_message('-442090041', input_text[x:x+4096])
+            update.message.reply_text(input_text[x:x+4096])
     else:
-        context.bot.send_message('-442090041', input_text)
-
-def tg_go(update, context):
-    global Is_monitoring_active
-    try:
-        Is_monitoring_active = True
-        url = update.message.text[len(TgCommands.Go)+2:]
-        x = Thread(target=tg_update, args=(update,context,))
-        x.start()
-        update.message.reply_text('started')
-    except Exception as e:
-        err_msg = "неизвестная ошибка: {0}".format(str(e))
-        update.message.reply_text(err_msg)
-
-def tg_stop(update, context):
-    global Is_monitoring_active
-    try:
-        Is_monitoring_active = False
-        update.message.reply_text('stopped')
-    except Exception as e:
-        err_msg = "неизвестная ошибка: {0}".format(str(e))
-        update.message.reply_text(err_msg)
-
-#--------------------------------------2--------------------------------------------------------
-def tg_zeroon(update, context):
-    global Is_zeroarmormode_active
-    try:
-        Is_zeroarmormode_active = True
-        update.message.reply_text('zero+')
-    except Exception as e:
-        err_msg = "неизвестная ошибка: {0}".format(str(e))
-        update.message.reply_text(err_msg)
+        update.message.reply_text(input_text)
         
-def tg_zerooff(update, context):
-    global Is_zeroarmormode_active
-    try:
-        Is_zeroarmormode_active = False
-        update.message.reply_text('zero-')
-    except Exception as e:
-        err_msg = "неизвестная ошибка: {0}".format(str(e))
-        update.message.reply_text(err_msg)
-
 #--------------------------------------main--------------------------------------------------------
 
 def main():
@@ -272,13 +337,11 @@ def main():
     updater = Updater("979411435:AAEHIVLx8L8CxmjIHtitaH4L1GeV_OCRJ7M", use_context=True)
     dp = updater.dispatcher
     
-    dp.add_handler(CommandHandler(TgCommands.Auto, tg_auto_teams))
     dp.add_handler(CommandHandler(TgCommands.Go, tg_go))
     dp.add_handler(CommandHandler(TgCommands.Stop, tg_stop))
-    dp.add_handler(CommandHandler(TgCommands.Set, tg_set))
-    dp.add_handler(CommandHandler(TgCommands.Game, tg_p_game))
-    dp.add_handler(CommandHandler(TgCommands.Zeroon, tg_zeroon))
-    dp.add_handler(CommandHandler(TgCommands.Zerooff, tg_zerooff))
+    dp.add_handler(CommandHandler(TgCommands.Game, tg_game))
+    dp.add_handler(CommandHandler(TgCommands.LvlStat, tg_lvl_stat))
+    dp.add_handler(CommandHandler(TgCommands.Points, tg_points_stat))
     #dp.add_handler(MessageHandler(Filters.text, tg_p_default))
     
     dp.add_error_handler(tg_error)
